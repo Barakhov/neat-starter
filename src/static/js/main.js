41cc984bf3b5683fc1e1c2608cd644681e1e1c11
Alex Barakhov <alexbarakhov@redradix.com>
2023-11-22 13:31:35 +0100

    dark mode

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,4 @@
+/*
 barba.use(barbaCss)
 
 barba.init({
@@ -14,3 +15,27 @@ barba.init({
     }
   ]
 })
+*/
+
+// Target an HTML element with a specific ID
+const htmlTag = document.getElementById("html");
+
+const btn = document.querySelector(".btn-toggle");
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark" || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  htmlTag.classList.add("dark");
+} else {
+  htmlTag.classList.remove("dark");
+}
+
+btn.addEventListener("click", function () {
+  htmlTag.classList.toggle("dark");
+
+  let theme = "light";
+  if (htmlTag.classList.contains("dark")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const settingsScreens = require('./tailwind.settings.screens')
 const settingsFontSizes = require('./tailwind.settings.fontSizes')
 
 module.exports = {
+  darkMode: 'class',
   content: ['./**/*.html'],
   theme: {
     screens: settingsScreens,
